4c11e3fe85b9822d5fdf28e804a8bcf393076a0b
ninjalombax <godzillakotm80@gmail.com>
2021-02-26 14:14:03 -0600

    Initial commit

--- CoinFlip WatchKit Extension/InterfaceController.swift
+++ CoinFlip WatchKit Extension/InterfaceController.swift
@@ -11,10 +11,42 @@ import Foundation
 
 class InterfaceController: WKInterfaceController {
 
+    @IBOutlet var flipLabel: WKInterfaceLabel!
     override func awake(withContext context: Any?) {
         // Configure interface objects here.
     }
     
+    @IBAction func flipButtonPressed() {
+        
+        self.flipLabel.setText("Flipping the Coin.")
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.5){
+            
+            self.flipLabel.setText("Flipping the Coin..")
+            
+        
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.5){
+            
+            self.flipLabel.setText("Flipping the Coin...")
+            
+        
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.5){
+            
+            let randomFlip = Bool.random()
+            
+            if(randomFlip)
+            {
+                self.flipLabel.setText("It's Heads!")
+            }else{
+                
+                self.flipLabel.setText("It's Tails!")
+            }
+        }
+        }
+        }
+    }
     override func willActivate() {
         // This method is called when watch view controller is about to be visible to user
     }
